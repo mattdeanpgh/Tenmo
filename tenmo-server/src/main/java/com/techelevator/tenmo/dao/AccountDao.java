c85c04905229d0f3ef933452ae69b2dc86a65bcb
Carrie <carriebea@gmail.com>
2022-07-06 13:38:14 -0400

    account DAO

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -0,0 +1,24 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Account;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface AccountDao {
+
+
+        List<Account> getAllAccounts();
+
+        BigDecimal getBalanceByUserId(int id);
+
+        BigDecimal getBalanceByAcctId(int acctId);
+
+        void updateBalance(BigDecimal balance, int acctId);
+
+        void addAccount(int userId);
+
+        void deleteAccount(int acctId);
+
+    }
+
